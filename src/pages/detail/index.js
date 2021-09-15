98249a161678e5ebb888c8d1ad5acc83b378b3bd
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2021-09-15 00:58:34 -0300

    useparams added to detail page

--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
+import { useParams, Redirect } from 'react-router';
 
 const PetDetailsPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const id = '51322435'; // <--- Update me!
+  let { id } = useParams(); // <--- Update me!
 
   useEffect(() => {
     async function getPetsData() {
@@ -30,6 +31,7 @@ const PetDetailsPage = () => {
       ) : error ? (
         <div>
           {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Redirect to="/pet-details-not-found" />
         </div>
       ) : (
         <main>
