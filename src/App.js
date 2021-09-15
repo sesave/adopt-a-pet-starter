449a9a299d5c6730a196cbd07ce01d0227b1e3a3
Sebastian Sanchez Vega <sebastiansanchezvega@gmail.com>
2021-09-15 00:21:17 -0300

    param added to home

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
 import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Navigation from './components/navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
+    <Router>
+      <Route to="/:type?">
+        <HomePage />
+      </Route>
       <Navigation />
-      <HomePage />
-    </div>
+    </Router>
   );
 }
 
